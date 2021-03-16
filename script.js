dbc474a28a0159fe2ae54b20547ff54ddfbc9506
Simona Cinti <simona.cinti2@gmail.com>
2021-03-16 19:16:49 +0100

    Winner is / Finish the game

--- script.js
+++ script.js
@@ -33,13 +33,14 @@ const diceImg = document.querySelector('.dice');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let play = true;
 
 score0Player.textContent = 0;
 score1Player.textContent = 0;
 diceImg.classList.add('none');
 
 /**
- * Tutorial
+ * Tutorial Part
  */
 const openModal = function () {
     modal.classList.remove('hidden');
@@ -62,48 +63,89 @@ btnCloseModal.addEventListener('click', closeModal);
 
 btnRoll.addEventListener('click', function(){
 
-    // generate a random roll from 1 to 6
-    const dice = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+    if (play){
 
-    // display the roll
-
-    diceImg.classList.remove('none');
-
-    const diceRolled = document.getElementById('dice-rolled');
-    switch (dice) {
-        case 1: 
-        diceRolled.className = 'fas fa-dice-one';
-        break;
-        case 2: 
-        diceRolled.className = 'fas fa-dice-two';
-        break;
-        case 3: 
-        diceRolled.className = 'fas fa-dice-three';
-        break;
-        case 4: 
-        diceRolled.className = 'fas fa-dice-four';
-        break;
-        case 5: 
-        diceRolled.className = 'fas fa-dice-five';
-        break;
-        case 6: 
-        diceRolled.className = 'fas fa-dice-six';
-        break;
-    }
-    console.log(diceRolled);
+        // generate a random roll from 1 to 6
+        const dice = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
     
-    // check if rolled 1: if true, go to next player and delete score
-
-    if(dice !== 1){
-        //Add dice to current score
-        currentScore +=  dice;
-        document.getElementById(`current-${activePlayer}`).textContent = currentScore;
-    }else{
-        // switch to other player
-        document.getElementById(`current-${activePlayer}`).textContent = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        currentScore = 0;
-        player0El.classList.toggle('player-active');
-        player1El.classList.toggle('player-active');
+        // display the roll
+        diceImg.classList.remove('none');
+    
+        const diceRolled = document.getElementById('dice-rolled');
+        switch (dice) {
+            case 1: 
+            diceRolled.className = 'fas fa-dice-one';
+            break;
+            case 2: 
+            diceRolled.className = 'fas fa-dice-two';
+            break;
+            case 3: 
+            diceRolled.className = 'fas fa-dice-three';
+            break;
+            case 4: 
+            diceRolled.className = 'fas fa-dice-four';
+            break;
+            case 5: 
+            diceRolled.className = 'fas fa-dice-five';
+            break;
+            case 6: 
+            diceRolled.className = 'fas fa-dice-six';
+            break;
+        }
+        // check if rolled 1: if true, go to next player and delete score
+    
+        if(dice !== 1){
+            //Add dice to current score
+            currentScore +=  dice;
+            document.getElementById(`current-${activePlayer}`).textContent = currentScore;
+        }else{
+            // switch to other player
+            changePlayer();
+        }
     }
-})
+    
+});
+
+/**
+ * Hold the score
+ */
+
+btnHold.addEventListener('click', function(){
+
+    if (play) {
+
+        // Add score to the active player score
+        scores[activePlayer] += currentScore; 
+        //check if player win (score >= 100)
+        document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
+    
+        // Check
+    
+        if (scores[activePlayer] >= 10) {
+            play = false;
+            diceImg.classList.add('none');
+            document.querySelector(`.player-${activePlayer}`).classList.add('player-winner');
+            document.querySelector(`.player-${activePlayer}`).classList.remove('player-active');
+            
+        } else {
+            // switch to the next player
+            changePlayer();
+    
+        }
+    };
+
+});
+
+/**
+ * FUNCTION
+ */
+
+
+// Change player
+function changePlayer(){
+    document.getElementById(`current-${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player-active');
+    player1El.classList.toggle('player-active');
+}

786de6d7489c67adb7b1f0551ee65f751e32da6d
Simona Cinti <simona.cinti2@gmail.com>
2021-03-16 18:26:17 +0100

    ready player 1

--- script.js
+++ script.js
@@ -22,9 +22,17 @@ const btnHold = document.querySelector('.btn-hold');
  * Variable for starting game
  */
 
+const player0El = document.querySelector('.player-0');
+const player1El = document.querySelector('.player-1');
 const score0Player = document.getElementById('score-0');
 const score1Player = document.getElementById('score-1');
+const current0El = document.getElementById('current-0');
+const current1El = document.getElementById('current-1');
 const diceImg = document.querySelector('.dice');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 score0Player.textContent = 0;
 score1Player.textContent = 0;
@@ -56,7 +64,6 @@ btnRoll.addEventListener('click', function(){
 
     // generate a random roll from 1 to 6
     const dice = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-    console.log(dice);
 
     // display the roll
 
@@ -86,4 +93,17 @@ btnRoll.addEventListener('click', function(){
     console.log(diceRolled);
     
     // check if rolled 1: if true, go to next player and delete score
+
+    if(dice !== 1){
+        //Add dice to current score
+        currentScore +=  dice;
+        document.getElementById(`current-${activePlayer}`).textContent = currentScore;
+    }else{
+        // switch to other player
+        document.getElementById(`current-${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
+        player0El.classList.toggle('player-active');
+        player1El.classList.toggle('player-active');
+    }
 })
